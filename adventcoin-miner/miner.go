6a0fbdc8fee58308fd5e3d69ec2e1d35fdf6cb58
arnoldadero <arnold.adero@hotmail.com>
2024-09-05 17:57:54 +0300

    Implement AdventCoin mining logic using MD5 hashing

--- adventcoin-miner/miner.go
+++ adventcoin-miner/miner.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"strings"
+)
+
+// MineAdventCoin finds the lowest number which when combined with the secret key
+// results in an MD5 hash that starts with 'prefixLength' number of zeroes.
+func MineAdventCoin(secretKey string, prefixLength int) (int, error) {
+	prefix := strings.Repeat("0", prefixLength)
+	for i := 1; i < 10000000; i++ {
+		// Concatenate secretKey with the current number i
+		data := fmt.Sprintf("%s%d", secretKey, i)
+		hash := md5.Sum([]byte(data))
+		hashStr := hex.EncodeToString(hash[:])
+
+		if strings.HasPrefix(hashStr, prefix) {
+			return i, nil
+		}
+	}
+	return 0, errors.New("no valid hash found")
+}
