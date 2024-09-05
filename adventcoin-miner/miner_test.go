7b362a83bb44c6ea3d72f1ab4ef94c16a02bf791
arnoldadero <arnold.adero@hotmail.com>
2024-09-05 17:58:50 +0300

    Test: test cases for AdventCoin mining logic

--- adventcoin-miner/miner_test.go
+++ adventcoin-miner/miner_test.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestMineAdventCoin(t *testing.T) {
+	testCases := []struct {
+		secretKey    string
+		prefixLength int
+		expected     int
+	}{
+		{"abcdef", 5, 609043},
+		{"pqrstuv", 5, 1048970},
+	}
+
+	for _, tc := range testCases {
+		result, err := MineAdventCoin(tc.secretKey, tc.prefixLength)
+		if err != nil {
+			t.Fatalf("Expected no error, got %v", err)
+		}
+		if result != tc.expected {
+			t.Errorf("For secretKey %s, expected %d but got %d", tc.secretKey, tc.expected, result)
+		}
+	}
+}
