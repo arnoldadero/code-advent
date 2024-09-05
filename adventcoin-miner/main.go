7b5d95363cf87ab403b8bd85be41c26f39459c7a
arnoldadero <arnold.adero@hotmail.com>
2024-09-05 17:57:07 +0300

    Feat:main.go entry point to run the miner

--- adventcoin-miner/main.go
+++ adventcoin-miner/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	secretKey := "iwrupvqb"
+	result, err := MineAdventCoin(secretKey, 5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("The lowest number for secret key %s is: %d\n", secretKey, result)
+}
